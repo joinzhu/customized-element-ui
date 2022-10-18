67c6d075c513d1bcec7d211dd7c963542d98cfad
join.zhu <joinzhufight@163.com>
2022-10-18 17:29:39 +0800

    修改工具库地址 -1

--- src/components/ExchangeDoc.vue
+++ src/components/ExchangeDoc.vue
@@ -8,7 +8,7 @@ export default {
   name: 'ExchangeDoc',
   methods: {
     handleClick() {
-      location.href = 'https://joinzhu.github.io/customized-tools-set/validate'
+      location.href = 'https://joinzhu.github.io/customized-tools-set/'
     }
   }
 }
